c622460056c974f2cac079d67aead677a42c734f
jhonsgomez <jhonsebastiangomezsierra@gmail.com>
2023-08-16 17:32:07 -0500

    version 1.0

---
+++ config/middlewares.ts
@@ -0,0 +1,12 @@
+export default [
+  "strapi::errors",
+  "strapi::security",
+  "strapi::poweredBy",
+  "strapi::cors",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
